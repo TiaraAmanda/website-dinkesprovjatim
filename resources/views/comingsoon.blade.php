a6dd34503a159f6d68ce6b3844f408eca631bb21
bagasroberto <roberto.bagas7@gmail.com>
2022-02-02 11:16:13 +0700

    update2

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html class="no-js" lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coming Soon</title>
+
+   <meta charset="utf-8">
+	<title>Coming Soon</title>
+   <meta name="description" content="">  
+   <meta name="author" content="">
+
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+   <link rel="stylesheet" href="{{asset('css/base.css')}}">
+   <link rel="stylesheet" href="{{asset('css/main.css')}}">   
+
+   <link rel="shortcut icon" href="#" >
+    
 </head>
+    
 <body>
-    <h2> COMING SOON :) </h2>
+
+   <div id="content-wrap">
+      <main class="row">
+            <header class="site-header"> 
+            </header>
+
+            <div id="main-content" class="twelve columns">
+               <br><br><br><br><br><br>
+               <h1>COMING SOON</h1>
+               <p> Website Is In Progress :)
+               </p>
+            </div>            
+      </main>	     
+   </div> 
 </body>
 </html>
